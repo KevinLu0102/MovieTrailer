e62789fc9d7eb384bb8367726c11c981656b8d9d
KevinLu <kevinlu0102@gmail.com>
2024-06-11 17:01:00 +0800

    Initial Commit

---
+++ MovieTrailer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MovieTrailer
+//
+//  Created by Kevin Lu on 2024/6/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MovieTrailer/MovieTrailerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieTrailerApp.swift
+//  MovieTrailer
+//
+//  Created by Kevin Lu on 2024/6/11.
+//
+
+import SwiftUI
+
+@main
+struct MovieTrailerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
